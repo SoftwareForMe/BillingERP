0c377f046f0ac440ecda8993477d88fb1a82d9f2
SoftwareForMe <kartavyabanna6@gmail.com>
2025-03-25 23:08:57 +0530

    Add files via upload

---
+++ PatientManagementSystem/config/config.js
@@ -0,0 +1,6 @@
+// Configuration file for the application
+module.exports = {
+    dbURI: 'sqlite:./database.sqlite', // Placeholder for SQLite
+    whatsappAPIKey: 'your_whatsapp_api_key',
+    paymentGatewayKey: 'your_payment_gateway_key'
+};
